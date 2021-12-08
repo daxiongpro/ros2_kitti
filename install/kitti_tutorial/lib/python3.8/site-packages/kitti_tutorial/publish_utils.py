ecda342a48c75e45f387633af2039dd5f0ecf597
daxiongpro <910660298@qq.com>
2021-12-08 16:14:40 +0800

    把点云publish代码放进内部类

--- install/kitti_tutorial/lib/python3.8/site-packages/kitti_tutorial/publish_utils.py
+++ install/kitti_tutorial/lib/python3.8/site-packages/kitti_tutorial/publish_utils.py
@@ -46,60 +46,64 @@ def publish_camera(cam_pub, bridge, image, borders_2d_cam2s=None, object_types=N
 
     image_temp = bridge.cv2_to_imgmsg(image, "bgr8")
     # header = Header(stamp=rospy.Time.now())
-    image_temp.header.frame_id = 'map'
+    image_temp.header.frame_id = FRAME_ID
     cam_pub.publish(image_temp)
 
-def create_point_cloud(points, parent_frame):
-    """ Creates a point cloud message.
-    Args:
-        points: Nx3 array of xyz positions.
-        parent_frame: frame in which the point cloud is defined
-    Returns:
-        sensor_msgs/PointCloud2 message
 
-    Code source:
-        https://gist.github.com/pgorczak/5c717baa44479fa064eb8d33ea4587e0
-
-    References:
-        http://docs.ros.org/melodic/api/sensor_msgs/html/msg/PointCloud2.html
-        http://docs.ros.org/melodic/api/sensor_msgs/html/msg/PointField.html
-        http://docs.ros.org/melodic/api/std_msgs/html/msg/Header.html
-
-    """
-    # In a PointCloud2 message, the point cloud is stored as an byte 
-    # array. In order to unpack it, we also include some parameters 
-    # which desribes the size of each individual point.
- 
- 
-    ros_dtype = sensor_msgs.PointField.FLOAT32
-    dtype = np.float32
-    itemsize = np.dtype(dtype).itemsize # A 32-bit float takes 4 bytes.
-
-    data = points.astype(dtype).tobytes() 
-
-    # The fields specify what the bytes represents. The first 4 bytes 
-    # represents the x-coordinate, the next 4 the y-coordinate, etc.
-    fields = [sensor_msgs.PointField(
-        name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
-        for i, n in enumerate('xyz')]
-
-    # The PointCloud2 message also has a header which specifies which 
-    # coordinate frame it is represented in. 
-    header = std_msgs.Header(frame_id=parent_frame)
-
-    return sensor_msgs.PointCloud2(
-        header=header,
-        height=1, 
-        width=points.shape[0],
-        is_dense=False,
-        is_bigendian=False,
-        fields=fields,
-        point_step=(itemsize * 3), # Every point consists of three float32s.
-        row_step=(itemsize * 3 * points.shape[0]),
-        data=data
-    )
 
 def publish_point_cloud(pcl_pub, point_cloud):
+
+    def create_point_cloud(points, parent_frame):
+        """ Creates a point cloud message.
+        Args:
+            points: Nx3 array of xyz positions.
+            parent_frame: frame in which the point cloud is defined
+        Returns:
+            sensor_msgs/PointCloud2 message
+
+        Code source:
+            https://gist.github.com/pgorczak/5c717baa44479fa064eb8d33ea4587e0
+
+        References:
+            http://docs.ros.org/melodic/api/sensor_msgs/html/msg/PointCloud2.html
+            http://docs.ros.org/melodic/api/sensor_msgs/html/msg/PointField.html
+            http://docs.ros.org/melodic/api/std_msgs/html/msg/Header.html
+
+        """
+        # In a PointCloud2 message, the point cloud is stored as an byte 
+        # array. In order to unpack it, we also include some parameters 
+        # which desribes the size of each individual point.
+    
+    
+        ros_dtype = sensor_msgs.PointField.FLOAT32
+        dtype = np.float32
+        itemsize = np.dtype(dtype).itemsize # A 32-bit float takes 4 bytes.
+
+        data = points.astype(dtype).tobytes() 
+
+        # The fields specify what the bytes represents. The first 4 bytes 
+        # represents the x-coordinate, the next 4 the y-coordinate, etc.
+        fields = [sensor_msgs.PointField(
+            name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
+            for i, n in enumerate('xyz')]
+
+        # The PointCloud2 message also has a header which specifies which 
+        # coordinate frame it is represented in. 
+        header = std_msgs.Header(frame_id=parent_frame)
+
+        return sensor_msgs.PointCloud2(
+            header=header,
+            height=1, 
+            width=points.shape[0],
+            is_dense=False,
+            is_bigendian=False,
+            fields=fields,
+            point_step=(itemsize * 3), # Every point consists of three float32s.
+            row_step=(itemsize * 3 * points.shape[0]),
+            data=data
+        )
+
+
     # header = Header()
     # header.stamp = rospy.Time.now()
 
