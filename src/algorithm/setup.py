a648b73e772141488c92ce6ddd67ec97b96fc7f3
daxiongpro <910660298@qq.com>
2021-12-10 10:44:45 +0800

    add algorithm node and init listener code

---
+++ src/algorithm/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'algorithm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daxiongpro',
+    maintainer_email='910660298@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "listener:algorithm.readpc2.py:main"
+        ],
+    },
+)
